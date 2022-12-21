0fa45449d6baafd5b2cd8a2b1d0f1d1840a6dbec
Liubov Gayduk <serenity0506@yandex.ru>
2022-12-22 01:12:58 +0300

    добавили запрос на показ и изменение карточки, обновили логику js через добавление класса

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
-const $wr = document.querySelector('[data-wr]');
-const $popup = document.forms.popup; //обращаемся к форме открытой
+import { CatsService } from "../components/CatsService.js";
+
+
+/// Objects and elements
+const $cardsContainer = document.querySelector('[data-wr]');
 const $popupWr = document.querySelector('[data-popupWr]'); //обращаемся к попапу
-const $popupContent = document.querySelector('[data-popupContent]');
+const $popupContentTemplate = document.querySelector('#popup-content-template').content;
+
 
 const ACTIONS = {
     DETAIL: 'detail',
@@ -9,55 +13,45 @@ const ACTIONS = {
     EDIT: 'edit'
 }
 
-const getCatHTML = (cat) => { //код добавления карточки кота
+const base_url = 'https://cats.petiteweb.dev/api/single/Serenity0506';
+const catsService = new CatsService(base_url);
+
+//код добавления содержимого попапа
+const getCreateCatHTMLElement = () => {
+    return $popupContentTemplate.cloneNode(true).firstElementChild;
+}
+
+//код добавления карточки кота
+const getCatHTML = (cat) => {
     return `
     <div data-cat-id=${cat.id} class="card">
     <div class="card__img_container">
         <img data-action=${ACTIONS.DELETE} class="card__delete" src="./images/trash-icon.png" alt="Удалить">
-        <img class="card__img" src="${cat.image}"
-            alt=${cat.name}></div>
+        <img class="card__img" src="${cat.image}" onerror="this.src='./images/fallback-img.jpg'"
+            alt="${cat.name}"></div>
         <div class="card__footer">
-            <h4 class="card__title">${cat.name}</h4>
+            <h4 class="card__title">${cat.favorite ? '😻' : '🙀'} ${cat.name}</h4>
+            <p class="card__text">${'⭐'.repeat(cat.rate)}</p>
             <p class="card__text">${cat.description}</p>
+            
             <div class="card__button">
                 <button data-action=${ACTIONS.DETAIL} type="button" class="card__button_detail">Детали</button>
-                <button data-action=${ACTIONS.EDIT} type="button" class="card__button_edit">Изменить</button>
             </div>
         </div>
     </div>
     `
 }
 
-fetch('https://cats.petiteweb.dev/api/single/Serenity0506/show/') // отображение всех котов из базы
-    .then((res) => res.json()) //получаем от сервера тело ответа (промис тоже) через обр.к json
-    .then((data) => {
-        $wr.insertAdjacentHTML('afterbegin', data.map(cat => getCatHTML(cat)).join('')) //обращаемся к контейнеру &wr, ставим в начало, обращаемся к data(тк массив), методом map делаем из массива объектов - массив строк и записываем в пустую строку методом .join
-    })
-
-$wr.addEventListener('click', (e) => { //удаление путем делегирования 
-    if (e.target.dataset.action === ACTIONS.DELETE) {
-        console.log(e.target)
-
-        const $catWr = e.target.closest('[data-cat-id]') //обращаемся к кнопке, а потом к ее родителю
-        const catId = $catWr.dataset.catId //записали айди кота в переменную
-
-        console.log({ catId })
-
-        fetch(`https://cats.petiteweb.dev/api/single/Serenity0506/delete/${catId}`, { //делаем запрос на сервер на удаление
-            method: 'DELETE',
-        })
-            .then(res => {
-                if (res.status === 200) {
-                    return $catWr.remove() //если тру, то ретерн зааершит ф-цию
-                }
-
-                alert(`Удаление кота с id = ${catId.id} не удалось`) //или выведет алерт
-            })
-    }
+/// Functions
+//отображение всех котов из базы
+catsService.getAllCats().then((cats) => {
+    $cardsContainer.insertAdjacentHTML('afterbegin', cats.map(cat => getCatHTML(cat)).join(''))
+    //обращаемся к контейнеру &wr, ставим в начало, обращаемся к cat(тк массив), 
+    //методом map делаем из массива объектов - массив строк и записываем в пустую строку методом .join
 })
 
-
-const submitPopupHandler = (e) => {
+//создание кота
+const createCatCard = (e) => {
     e.preventDefault()
 
     let popupDataObject = Object.fromEntries(new FormData(e.target).entries());
@@ -71,16 +65,10 @@ const submitPopupHandler = (e) => {
     }
 
     //запрос на сервер на отправку данных на создание кота
-    fetch('https://cats.petiteweb.dev/api/single/Serenity0506/add/', {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json' //этим загловком мы сообщаем серверу, что отсылаем данные в формате json
-        },
-        body: JSON.stringify(popupDataObject) //тело запроса - превращаем объект в строку, чтобы ее передать по http в формате json
-    })
-        .then((res) => {
-            if (res.status === 200) {
-                return $wr.insertAdjacentHTML(
+    catsService.addCat(popupDataObject)
+        .then((isOk) => {
+            if (isOk) {
+                return $cardsContainer.insertAdjacentHTML(
                     'afterbegin',
                     getCatHTML(popupDataObject)
                 ) //обращаемся к контейнеру &wr, ставим в начало, добавляем кота 
@@ -88,43 +76,131 @@ const submitPopupHandler = (e) => {
             throw Error('Ошибка при создании кота') //показывается, если были ответы не 200, но тоже валидные
         }).catch(alert) //показывается, если фетч сломался
 }
-//нужно сделать валидацию данных (1:12 тайминг)
 
-const clickPopupWrHandler = (e) => {
+//удаление карточки кота путем делегирования
+$cardsContainer.addEventListener('click', (e) => {
+    const ds = e.target.dataset;
+
+    if (ds.action) {
+        const $catWr = e.target.closest('[data-cat-id]') //обращаемся к кнопке, а потом к ее родителю
+        const catId = $catWr.dataset.catId //записали айди кота в переменную
+
+        if (ds.action == ACTIONS.DELETE) {
+            deleteCatCard(catId, $catWr);
+        }
+        else if (ds.action == ACTIONS.DETAIL) {
+            openCatCard(catId);
+        }
+    }
+})
+
+function deleteCatCard(catId, cardElement) {
+    catsService.deleteCat(catId)
+        .then(isOk => { //вызвали метод делит из сервиса, взяли результат промиса isOk
+            if (isOk) {
+                return cardElement.remove() //если тру, то ретерн зааершит ф-цию
+            }
+
+            alert(`Удаление кота с id = ${catId} не удалось`) //или выведет алерт
+        });
+}
+
+function openCatCard(catId) {
+    $popupWr.innerHTML = ''
+    $popupWr.addEventListener('click', closePopupOnBackgroundClick)
+    $popupWr.prepend(getCreateCatHTMLElement())
+
+    const cat = catsService.getCatFromStorage(catId)
+
+    for (let inputName in cat) {
+        let inputElement = document.forms.popup.elements.namedItem(inputName);
+        inputElement.value = cat[inputName];
+
+        if (inputElement.name == 'id' || inputElement.name == 'name') {
+            inputElement.disabled = 'disabled'
+        }
+    }
+
+    $popupWr.classList.remove('popup__invisible')
+    const $popup = document.forms.popup;
+    $popup.addEventListener('submit', updateCatCard) //добавили обработчик событий
+
+}
+
+const updateCatCard = (e) => {
+    e.preventDefault()
+
+    let catData = {}
+
+    for (let el of document.forms.popup.elements) {
+        if (el.tagName == 'INPUT' || el.tagName == 'TEXTAREA') {
+            catData[el.name] = el.value
+        }
+    }
+
+    catsService.editCat(catData)
+        .then((isOk) => {
+            if (isOk) {
+                // нашли карточку, которую редактировали
+                const editedCardElement = document.querySelector(`[data-cat-id="${catData.id}"]`)
+
+                // Добавили карту с отредактированным котом перед существующей
+                editedCardElement.insertAdjacentHTML(
+                    'beforebegin',
+                    getCatHTML(catData)
+                )
+
+                // удалили существующую карту
+                editedCardElement.remove()
+
+                $popupWr.classList.add('popup__invisible')
+                $popupWr.removeEventListener('click', closePopupOnBackgroundClick)
+
+                return
+            }
+            throw Error('Ошибка при создании кота') //показывается, если были ответы не 200, но тоже валидные
+        }).catch(alert) //показывается, если фетч сломался
+}
+
+//закрытие попап кликом по фону
+const closePopupOnBackgroundClick = (e) => {
     if (e.target === $popupWr) {
         $popupWr.classList.add('popup__invisible')
-        $popupWr.removeEventListener('click', clickPopupWrHandler)
-        $popup.removeEventListener('submit', submitPopupHandler) //удалили обработчик событий    }
+        $popupWr.removeEventListener('click', closePopupOnBackgroundClick)
+        // $popup.removeEventListener('submit', submitPopupHandler) //удалили обработчик событий    }
     }
 }
 
-const openPopupHendler = (e) => { //добавляем обработчик события по клику на документ, чтобы открывать модалки
-    const targetPopupName = e.target.dataset.openpopup;
-
-    if (targetPopupName === 'createCat') {
-        $popupWr.classList.remove('popup__invisible')
-        $popupWr.addEventListener('click', clickPopupWrHandler)
-        $popup.addEventListener('submit', submitPopupHandler) //добавили обработчик событий
-    }
-} //ф-ция открытия модалки
-
-document.addEventListener('click', openPopupHendler) //по клику попап открыт
-
-document.addEventListener('keydown', (e) => {
-    console.log(e)
-
-    if (e.key === "Escape") {
-        $popupWr.classList.add('popup__invisible')
-        $popupWr.removeEventListener('click', clickPopupWrHandler)
-        $popup.removeEventListener('submit', submitPopupHandler) //удалили обработчик событий
-    }
-}
-);
-
-function handlePopupCloseButton(e) {
-    const closeButton = $popup.querySelector('.card__delete');
+//закрытие попап кликом по крестику
+const closePopupByCloseButton = (e) => {
+    const closeButton = $popupWr.querySelector('.card__delete');
     if (e.target === closeButton) {
         $popupWr.classList.add('popup__invisible')
     }
 }
-$popupWr.addEventListener('click', handlePopupCloseButton);
+$popupWr.addEventListener('click', closePopupByCloseButton);
+
+//закрытие попап по клавише esc
+document.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") {
+        $popupWr.classList.add('popup__invisible')
+        $popupWr.removeEventListener('click', closePopupOnBackgroundClick)
+    }
+}
+);
+
+//открытие попап
+const openPopupHendler = (e) => { //добавляем обработчик события по клику на документ, чтобы открывать модалки
+    const targetPopupName = e.target.dataset.openpopup;
+
+    if (targetPopupName === 'createCat') {
+        $popupWr.innerHTML = ''
+        $popupWr.addEventListener('click', closePopupOnBackgroundClick)
+        $popupWr.prepend(getCreateCatHTMLElement())
+        $popupWr.classList.remove('popup__invisible')
+        const $popup = document.forms.popup; //обращаемся к форме открытой
+        $popup.addEventListener('submit', createCatCard) //добавили обработчик событий
+    }
+} //ф-ция открытия модалки
+
+document.addEventListener('click', openPopupHendler) //по клику попап открыт
